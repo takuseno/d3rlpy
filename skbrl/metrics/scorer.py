Fix scorer sign

skbrl/metrics/scorer.py
@@ -20,7 +20,8 @@ def td_error_scorer(algo, episodes):
         y = batch.next_rewards + algo.gamma * next_values * mask
         total_errors += ((values - y)**2).reshape(-1).tolist()
 
-    return np.mean(total_errors)
+    # smaller is better
+    return -np.mean(total_errors)
 
 
 def discounted_sum_of_advantage_scorer(algo, episodes):
@@ -47,7 +48,8 @@ def discounted_sum_of_advantage_scorer(algo, episodes):
 
         total_sums += sum_advantages
 
-    return np.mean(total_sums)
+    # smaller is better
+    return -np.mean(total_sums)
 
 
 def evaluate_on_environment(env, n_trials=10):
@@ -66,3 +68,4 @@ def evaluate_on_environment(env, n_trials=10):
         return np.mean(episode_rewards)
 
     return scorer
+NEGATED_SCORER = [td_error_scorer, discounted_sum_of_advantage_scorer]

skbrl/tests/metrics/test_scorer.py
@@ -59,7 +59,8 @@ def test_td_error_scorer(observation_shape, action_size, n_episodes,
                                        gamma)
         ref_errors += ref_error
 
-    assert np.allclose(td_error_scorer(algo, episodes), np.mean(ref_errors))
+    score = td_error_scorer(algo, episodes)
+    assert np.allclose(score, -np.mean(ref_errors))
 
 
 def ref_discounted_sum_of_advantage_score(predict_value, observations,
@@ -108,8 +109,8 @@ def test_discounted_sum_of_advantage_scorer(observation_shape, action_size,
             policy_actions, gamma)
         ref_sums += ref_sum
 
-    sums = discounted_sum_of_advantage_scorer(algo, episodes)
-    assert np.allclose(sums, np.mean(ref_sums))
+    score = discounted_sum_of_advantage_scorer(algo, episodes)
+    assert np.allclose(score, -np.mean(ref_sums))
 
 
 @pytest.mark.parametrize('observation_shape', [(100, )])