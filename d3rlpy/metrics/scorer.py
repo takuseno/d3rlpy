3e1f101bd8884f72ba05032ccf7ef88910500af4
takuseno <takuma.seno@gmail.com>
2020-07-16 15:14:51 +0900

    Fix memory allocation issue at metrics

--- d3rlpy/metrics/scorer.py
+++ d3rlpy/metrics/scorer.py
@@ -3,62 +3,74 @@ import numpy as np
 from ..dataset import TransitionMiniBatch
 
 
-def td_error_scorer(algo, episodes):
+def _make_batches_from_episode(episode, window_size):
+    n_batches = len(episode) // window_size
+    if len(episode) % window_size != 0:
+        n_batches += 1
+    for i in range(n_batches):
+        head_index = i * window_size
+        last_index = min(head_index + window_size, len(episode))
+        transitions = episode.transitions[head_index:last_index]
+        batch = TransitionMiniBatch(transitions)
+        yield batch
+
+
+def td_error_scorer(algo, episodes, window_size=100):
     total_errors = []
     for episode in episodes:
-        batch = TransitionMiniBatch(episode.transitions)
+        for batch in _make_batches_from_episode(episode, window_size):
+            # estimate values for current observations
+            values = algo.predict_value(batch.observations, batch.actions)
 
-        # estimate values for current observations
-        values = algo.predict_value(batch.observations, batch.actions)
+            # estimate values for next observations
+            next_actions = algo.predict(batch.next_observations)
+            next_values = algo.predict_value(batch.next_observations,
+                                             next_actions)
 
-        # estimate values for next observations
-        next_actions = algo.predict(batch.next_observations)
-        next_values = algo.predict_value(batch.next_observations, next_actions)
-
-        # calculate td errors
-        mask = (1.0 - batch.terminals)
-        y = batch.next_rewards + algo.gamma * next_values * mask
-        total_errors += ((values - y)**2).reshape(-1).tolist()
+            # calculate td errors
+            mask = (1.0 - batch.terminals)
+            y = batch.next_rewards + algo.gamma * next_values * mask
+            total_errors += ((values - y)**2)[:, 0].tolist()
 
     # smaller is better
     return -np.mean(total_errors)
 
 
-def discounted_sum_of_advantage_scorer(algo, episodes):
+def discounted_sum_of_advantage_scorer(algo, episodes, window_size=100):
     total_sums = []
     for episode in episodes:
-        batch = TransitionMiniBatch(episode.transitions)
+        for batch in _make_batches_from_episode(episode, window_size):
+            # estimate values for dataset actions
+            dataset_values = algo.predict_value(batch.observations,
+                                                batch.actions)
 
-        # estimate values for dataset actions
-        dataset_values = algo.predict_value(batch.observations, batch.actions)
+            # estimate values for the current policy
+            actions = algo.predict(batch.observations)
+            on_policy_values = algo.predict_value(batch.observations, actions)
 
-        # estimate values for the current policy
-        actions = algo.predict(batch.observations)
-        on_policy_values = algo.predict_value(batch.observations, actions)
+            # calculate advantages
+            advantages = (dataset_values - on_policy_values)[:, 0].tolist()
 
-        # calculate advantages
-        advantages = (dataset_values - on_policy_values).reshape(-1).tolist()
+            # calculate discounted sum of advantages
+            A = advantages[-1]
+            sum_advantages = [A]
+            for advantage in reversed(advantages[:-1]):
+                A = advantage + algo.gamma * A
+                sum_advantages.append(A)
 
-        # calculate discounted sum of advantages
-        A = advantages[-1]
-        sum_advantages = [A]
-        for advantage in reversed(advantages[:-1]):
-            A = advantage + algo.gamma * A
-            sum_advantages.append(A)
-
-        total_sums += sum_advantages
+            total_sums += sum_advantages
 
     # smaller is better
     return -np.mean(total_sums)
 
 
-def average_value_estimation_scorer(algo, episodes):
+def average_value_estimation_scorer(algo, episodes, window_size=100):
     total_values = []
     for episode in episodes:
-        batch = TransitionMiniBatch(episode.transitions)
-        actions = algo.predict(batch.observations)
-        values = algo.predict_value(batch.observations, actions)
-        total_values += values.tolist()
+        for batch in _make_batches_from_episode(episode, window_size):
+            actions = algo.predict(batch.observations)
+            values = algo.predict_value(batch.observations, actions)
+            total_values += values[:, 0].tolist()
     return np.mean(total_values)
 
 
