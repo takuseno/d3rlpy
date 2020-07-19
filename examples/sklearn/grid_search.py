7b56c43a9bc4d0e9a12070e63fb01d28da262327
takuseno <takuma.seno@gmail.com>
2020-07-19 22:28:19 +0900

    Add grid search example

---
+++ examples/sklearn/grid_search.py
@@ -0,0 +1,26 @@
+from d3rlpy.algos import DQN
+from d3rlpy.datasets import get_cartpole
+from d3rlpy.metrics.scorer import evaluate_on_environment
+from d3rlpy.context import parallel
+from sklearn.model_selection import GridSearchCV
+
+# obtain dataset
+dataset, env = get_cartpole()
+
+# setup algowithm with GPU enabled
+dqn = DQN(n_epochs=1, use_gpu=True)
+
+# grid search with multiple GPUs assigned to individual processs
+with parallel():
+    env_score = evaluate_on_environment(env)
+    gscv = GridSearchCV(estimator=dqn,
+                        param_grid={
+                            'learning_rate': [1e-3, 3e-4, 1e-4],
+                            'gamma': [0.99, 0.95, 0.9]
+                        },
+                        scoring={'environment': env_score},
+                        refit=False,
+                        n_jobs=3)
+    gscv.fit(dataset.episodes, show_progress=False)
+
+print(gscv.grid_scores_)
