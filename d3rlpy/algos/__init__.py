678bf08aa5fd2935865e1be6b30e81a16d7f8464
takuseno <takuma.seno@gmail.com>
2020-07-20 19:08:22 +0900

    Add create_algo function

--- d3rlpy/algos/__init__.py
+++ d3rlpy/algos/__init__.py
@@ -7,3 +7,64 @@ from .ddpg import DDPG
 from .dqn import DQN, DoubleDQN
 from .sac import SAC
 from .td3 import TD3
+
+
+def create_algo(name, discrete, **params):
+    """ Returns algorithm from its name.
+
+    Args:
+        name (str): algorithm name in snake_case.
+        discrete (bool): flag to use discrete action-space algorithm.
+        params (any): arguments for algorithm.
+
+    Returns:
+        d3rlpy.algos.base.AlgoBase: algorithm.
+
+    """
+    if name == 'bc':
+        if discrete:
+            return DiscreteBC(**params)
+        else:
+            return BC(**params)
+    elif name == 'bcq':
+        if discrete:
+            return DiscreteBCQ(**params)
+        else:
+            return BCQ(**params)
+    elif name == 'bear':
+        if discrete:
+            raise ValueError('BEAR does not support discrete action-space.')
+        else:
+            return BEAR(**params)
+    elif name == 'cql':
+        if discrete:
+            return DiscreteCQL(**params)
+        else:
+            return CQL(**params)
+    elif name == 'ddpg':
+        if discrete:
+            raise ValueError('DDPG does not support discrete action-space.')
+        else:
+            return DDPG(**params)
+    elif name == 'dqn':
+        if discrete:
+            return DQN(**params)
+        else:
+            raise ValueError('DQN does not support continuous action-space.')
+    elif name == 'double_dqn':
+        if discrete:
+            return DoubleDQN(**params)
+        else:
+            raise ValueError(
+                'DoubleDQN does not support continuous action-space.')
+    elif name == 'sac':
+        if discrete:
+            raise ValueError('SAC does not support discrete action-space.')
+        else:
+            return SAC(**params)
+    elif name == 'td3':
+        if discrete:
+            raise ValueError('TD3 does not support discrete action-space.')
+        else:
+            return TD3(**params)
+    raise ValueError('invalid algorithm name.')
