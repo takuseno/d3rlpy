012b5e7c26c5be071625373ab6009debdc2b7a3b
takuseno <takuma.seno@gmail.com>
2020-07-30 22:06:33 +0900

    Add docstring to MOPO

--- d3rlpy/dynamics/mopo.py
+++ d3rlpy/dynamics/mopo.py
@@ -2,6 +2,75 @@ from .base import DynamicsBase
 
 
 class MOPO(DynamicsBase):
+    """ Model-based Offline Policy Optimization.
+
+    MOPO is a model-based RL approach for offline policy optimization.
+    MOPO leverages the probablistic ensemble dynamics model to generate
+    new dynamics data with uncertainty penalties.
+
+    The ensemble dynamics model consists of :math:`N` probablistic models
+    :math:`\{T_{\\theta_i}\}_{i=1}^N`.
+    At each epoch, new transitions are generated via randomly picked dynamics
+    model :math:`T_\\theta`.
+
+    .. math::
+
+        s_{t+1}, r_{t+1} \\sim T_\\theta(s_t, a_t)
+
+    where :math:`s_t \\sim D` for the first step, otherwise :math:`s_t` is the
+    previous generated observation, and :math:`a_t \\sim \\pi(\cdot|s_t)`.
+    The generated :math:`r_{t+1}` would be far from the ground truth if the
+    actions sampled from the policy function is out-of-distribution.
+    Thus, the uncertainty penalty reguralizes this bias.
+
+    .. math::
+
+        \\tilde{r_{t+1}} = r_{t+1} - \\lambda \\max_{i=1}^N
+            || \\Sigma_i (s_t, a_t) ||
+
+    where :math:`\\Sigma(s_t, a_t)` is the estimated variance.
+
+    Finally, the generated transitions
+    :math:`(s_t, a_t, \\tilde{r_{t+1}}, s_{t+1})` are appended to dataset
+    :math:`D`.
+
+    This generation process starts with randomly sampled `n_transitions`
+    transitions till `horizon` steps.
+
+    References:
+        * `Yu et al., MOPO: Model-based Offline Policy Optimization.
+          <https://arxiv.org/abs/2005.13239>`_
+
+    Args:
+        n_epochs (int): the number of epochs to train.
+        batch_size (int): mini-batch size.
+        learning_rate (float): learning rate for dynamics model.
+        eps (float): :math:`\\epsilon` for Adam optimizer.
+        n_ensembles (int): the number of dynamics model for ensemble.
+        n_transitions (int): the number of parallel trajectories to generate.
+        horizon (int): the number of steps to generate.
+        lam (float): :math:`\\lambda` for uncertainty penalties.
+        use_batch_norm (bool): flag to insert batch normalization layers.
+        scaler (d3rlpy.preprocessing.scalers.Scaler or str): preprocessor.
+            The available options are `['pixel', 'min_max', 'standard']`.
+        use_gpu (bool or d3rlpy.gpu.Device): flag to use GPU or device.
+        impl (d3rlpy.dynamics.base.DynamicsImplBase): dynamics implementation.
+
+    Attributes:
+        n_epochs (int): the number of epochs to train.
+        batch_size (int): mini-batch size.
+        learning_rate (float): learning rate for dynamics model.
+        eps (float): :math:`\\epsilon` for Adam optimizer.
+        n_ensembles (int): the number of dynamics model for ensemble.
+        n_transitions (int): the number of parallel trajectories to generate.
+        horizon (int): the number of steps to generate.
+        lam (float): :math:`\\lambda` for uncertainty penalties.
+        use_batch_norm (bool): flag to insert batch normalization layers.
+        scaler (d3rlpy.preprocessing.scalers.Scaler): preprocessor.
+        use_gpu (d3rlpy.gpu.Device): flag to use GPU or device.
+        impl (d3rlpy.dynamics.base.DynamicsImplBase): dynamics implementation.
+
+    """
     def __init__(self,
                  n_epochs=30,
                  batch_size=100,
@@ -9,8 +78,8 @@ class MOPO(DynamicsBase):
                  eps=1e-8,
                  n_ensembles=5,
                  n_transitions=10,
-                 lam=1.0,
                  horizon=5,
+                 lam=1.0,
                  use_batch_norm=False,
                  scaler=None,
                  use_gpu=False,
