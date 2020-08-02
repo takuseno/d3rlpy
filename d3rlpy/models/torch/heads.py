f0ada8d8dffa3fb981b041fb9c9aeeef50307056
takuseno <takuma.seno@gmail.com>
2020-08-02 17:40:59 +0900

    Add activation option to heads

--- d3rlpy/models/torch/heads.py
+++ d3rlpy/models/torch/heads.py
@@ -3,11 +3,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+def _create_activation(activation_type):
+    if activation_type == 'relu':
+        return torch.relu
+    elif activation_type == 'swish':
+        return lambda x: x * torch.sigmoid(x)
+    raise ValueError('invalid activation_type.')
+
+
 def create_head(observation_shape,
                 action_size=None,
                 use_batch_norm=False,
                 discrete_action=False,
+                activation_type='relu',
                 **kwargs):
+
+    activation = _create_activation(activation_type)
+
     if len(observation_shape) == 3:
         # pixel input
         if action_size is not None:
@@ -15,9 +27,11 @@ def create_head(observation_shape,
                                        action_size,
                                        use_batch_norm=use_batch_norm,
                                        discrete_action=discrete_action,
+                                       activation=activation,
                                        **kwargs)
         return PixelHead(observation_shape,
                          use_batch_norm=use_batch_norm,
+                         activation=activation,
                          **kwargs)
     elif len(observation_shape) == 1:
         # vector input
@@ -26,9 +40,11 @@ def create_head(observation_shape,
                                         action_size,
                                         use_batch_norm=use_batch_norm,
                                         discrete_action=discrete_action,
+                                        activation=activation,
                                         **kwargs)
         return VectorHead(observation_shape,
                           use_batch_norm=use_batch_norm,
+                          activation=activation,
                           **kwargs)
     else:
         raise ValueError('observation_shape must be 1d or 3d.')
@@ -39,7 +55,8 @@ class PixelHead(nn.Module):
                  observation_shape,
                  filters=None,
                  feature_size=None,
-                 use_batch_norm=False):
+                 use_batch_norm=False,
+                 activation=torch.relu):
         super().__init__()
 
         # default architecture is based on Nature DQN paper.
@@ -50,6 +67,7 @@ class PixelHead(nn.Module):
 
         self.observation_shape = observation_shape
         self.use_batch_norm = use_batch_norm
+        self.activation = activation
         self.feature_size = feature_size
 
         # convolutional layers
@@ -80,7 +98,7 @@ class PixelHead(nn.Module):
     def _conv_encode(self, x):
         h = x
         for i in range(len(self.convs)):
-            h = torch.relu(self.convs[i](h))
+            h = self.activation(self.convs[i](h))
             if self.use_batch_norm:
                 h = self.conv_bns[i](h)
         return h
@@ -88,7 +106,7 @@ class PixelHead(nn.Module):
     def forward(self, x):
         h = self._conv_encode(x)
 
-        h = torch.relu(self.fc(h.view(h.shape[0], -1)))
+        h = self.activation(self.fc(h.view(h.shape[0], -1)))
         if self.use_batch_norm:
             h = self.fc_bn(h)
 
@@ -102,11 +120,12 @@ class PixelHeadWithAction(PixelHead):
                  filters=None,
                  feature_size=None,
                  use_batch_norm=False,
-                 discrete_action=False):
+                 discrete_action=False,
+                 activation=torch.relu):
         self.action_size = action_size
         self.discrete_action = discrete_action
         super().__init__(observation_shape, filters, feature_size,
-                         use_batch_norm)
+                         use_batch_norm, activation)
 
     def _get_linear_input_size(self):
         size = super()._get_linear_input_size()
@@ -121,7 +140,7 @@ class PixelHeadWithAction(PixelHead):
 
         # cocat feature and action
         h = torch.cat([h.view(h.shape[0], -1), action], dim=1)
-        h = torch.relu(self.fc(h))
+        h = self.activation(self.fc(h))
         if self.use_batch_norm:
             h = self.fc_bn(h)
 
@@ -132,7 +151,8 @@ class VectorHead(nn.Module):
     def __init__(self,
                  observation_shape,
                  hidden_units=None,
-                 use_batch_norm=False):
+                 use_batch_norm=False,
+                 activation=torch.relu):
         super().__init__()
         self.observation_shape = observation_shape
 
@@ -141,6 +161,7 @@ class VectorHead(nn.Module):
 
         self.use_batch_norm = use_batch_norm
         self.feature_size = hidden_units[-1]
+        self.activation = activation
 
         in_units = [observation_shape[0]] + hidden_units[:-1]
         self.fcs = nn.ModuleList()
@@ -153,7 +174,7 @@ class VectorHead(nn.Module):
     def forward(self, x):
         h = x
         for i in range(len(self.fcs)):
-            h = torch.relu(self.fcs[i](h))
+            h = self.activation(self.fcs[i](h))
             if self.use_batch_norm:
                 h = self.bns[i](h)
         return h
@@ -165,11 +186,13 @@ class VectorHeadWithAction(VectorHead):
                  action_size,
                  hidden_units=None,
                  use_batch_norm=False,
-                 discrete_action=False):
+                 discrete_action=False,
+                 activation=torch.relu):
         self.action_size = action_size
         self.discrete_action = discrete_action
         concat_shape = (observation_shape[0] + action_size, )
-        super().__init__(concat_shape, hidden_units, use_batch_norm)
+        super().__init__(concat_shape, hidden_units, use_batch_norm,
+                         activation)
         self.observation_shape = observation_shape
 
     def forward(self, x, action):
