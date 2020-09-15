e975808980c19298ffd4f150baba177005c37d51
takuseno <takuma.seno@gmail.com>
2020-09-15 23:40:04 +0900

    Fix Cython error at readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,6 @@ import os
 import sys
 import pkg_resources
 
-sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
-
 # -- readthedocs -------------------------------------------------------------
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
