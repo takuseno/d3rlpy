9dbf527e1d48a1b1378f06e46b3036d3e0703e05
takuseno <takuma.seno@gmail.com>
2020-10-21 22:12:00 +0900

    Updaet online atari example based on the paper

--- examples/online/train_atari.py
+++ examples/online/train_atari.py
@@ -10,11 +10,13 @@ _, env = get_atari('breakout-mixed-v0')
 eval_env = copy.deepcopy(env)
 
 # setup algorithm
-dqn = DoubleDQN(n_epochs=10000,
-                batch_size=32,
-                learning_rate=2.5e-4,
-                target_update_interval=10000,
-                q_func_type='mean',
+dqn = DoubleDQN(batch_size=32,
+                learning_rate=5e-5,
+                eps=0.01,
+                target_update_interval=32000,
+                q_func_type='qr',
+                scaler='pixel',
+                n_frames=4,
                 use_gpu=True)
 
 # replay buffer for experience replay
@@ -22,15 +24,17 @@ buffer = ReplayBuffer(maxlen=1000000, env=env)
 
 # epilon-greedy explorer
 explorer = LinearDecayEpsilonGreedy(start_epsilon=1.0,
-                                    end_epsilon=0.1,
-                                    duration=1000000)
+                                    end_epsilon=0.01,
+                                    duration=4000000)
 
 # start training
 dqn.fit_online(env,
                buffer,
                explorer,
                eval_env=eval_env,
-               eval_epsilon=0.01,
+               eval_epsilon=0.0001,
+               eval_interval=100,
+               n_epochs=50000,
                n_steps_per_epoch=1000,
-               n_updates_per_epoch=1000,
-               update_start_step=50000)
+               n_updates_per_epoch=250,
+               update_start_step=200000)
