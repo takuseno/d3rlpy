c8d8d739b8e49bb2729efcec82407d12a810dbd0
takuseno <takuma.seno@gmail.com>
2020-10-28 22:41:54 +0900

    Update atari example to match DQN

--- examples/online/train_atari.py
+++ examples/online/train_atari.py
@@ -11,9 +11,8 @@ eval_env = copy.deepcopy(env)
 
 # setup algorithm
 dqn = DoubleDQN(batch_size=32,
-                learning_rate=5e-5,
-                eps=0.01,
-                target_update_interval=32000,
+                learning_rate=2.5e-4,
+                target_update_interval=10000 / 4,
                 q_func_type='qr',
                 scaler='pixel',
                 n_frames=4,
@@ -24,17 +23,18 @@ buffer = ReplayBuffer(maxlen=1000000, env=env)
 
 # epilon-greedy explorer
 explorer = LinearDecayEpsilonGreedy(start_epsilon=1.0,
-                                    end_epsilon=0.01,
-                                    duration=4000000)
+                                    end_epsilon=0.1,
+                                    duration=1000000)
 
 # start training
 dqn.fit_online(env,
                buffer,
                explorer,
                eval_env=eval_env,
-               eval_epsilon=0.0001,
+               eval_epsilon=0.01,
                eval_interval=100,
                n_epochs=50000,
                n_steps_per_epoch=1000,
                n_updates_per_epoch=250,
-               update_start_step=200000)
+               update_start_step=50000,
+               save_interval=1000)
