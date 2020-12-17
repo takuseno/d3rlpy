51be7f1f53660f87048202fb58dac35dff4d5c1c
takuseno <takuma.seno@gmail.com>
2020-12-17 21:16:51 +0900

    Refactor create_algo function

--- d3rlpy/algos/__init__.py
+++ d3rlpy/algos/__init__.py
@@ -1,4 +1,3 @@
-from .base import AlgoBase
 from .awac import AWAC
 from .awr import AWR, DiscreteAWR
 from .bc import BC, DiscreteBC
@@ -7,13 +6,56 @@ from .bear import BEAR
 from .cql import CQL, DiscreteCQL
 from .ddpg import DDPG
 from .dqn import DQN, DoubleDQN
-from .plas import PLAS, PLASWithPerturbation
+from .plas import PLASWithPerturbation
 from .sac import SAC, DiscreteSAC
 from .td3 import TD3
 
+DISCRETE_ALGORITHMS = {
+    'awr': DiscreteAWR,
+    'bc': DiscreteBC,
+    'bcq': DiscreteBCQ,
+    'cql': DiscreteCQL,
+    'dqn': DQN,
+    'double_dqn': DoubleDQN,
+    'sac': DiscreteSAC
+}
+
+CONTINUOUS_ALGORITHMS = {
+    'awac': AWAC,
+    'awr': AWR,
+    'bc': BC,
+    'bcq': BCQ,
+    'bear': BEAR,
+    'cql': CQL,
+    'ddpg': DDPG,
+    'sac': SAC,
+    'plas': PLASWithPerturbation,
+    'td3': TD3
+}
+
+
+def get_algo(name, discrete):
+    """ Returns algorithm class from its name.
+
+    Args:
+        name (str): algorithm name in snake_case.
+        discrete (bool): flag to use discrete action-space algorithm.
+
+    Returns:
+        type: algorithm class.
+
+    """
+    if discrete:
+        if name in DISCRETE_ALGORITHMS:
+            return DISCRETE_ALGORITHMS[name]
+        raise ValueError('%s does not support discrete action-space.' % name)
+    if name in CONTINUOUS_ALGORITHMS:
+        return CONTINUOUS_ALGORITHMS[name]
+    raise ValueError('%s does not support continuous action-space.' % name)
+
 
 def create_algo(name, discrete, **params):
-    """ Returns algorithm from its name.
+    """ Returns algorithm object from its name.
 
     Args:
         name (str): algorithm name in snake_case.
@@ -24,60 +66,4 @@ def create_algo(name, discrete, **params):
         d3rlpy.algos.base.AlgoBase: algorithm.
 
     """
-    if name == 'awr':
-        if discrete:
-            return DiscreteAWR(**params)
-        else:
-            return AWR(**params)
-    elif name == 'bc':
-        if discrete:
-            return DiscreteBC(**params)
-        else:
-            return BC(**params)
-    elif name == 'bcq':
-        if discrete:
-            return DiscreteBCQ(**params)
-        else:
-            return BCQ(**params)
-    elif name == 'bear':
-        if discrete:
-            raise ValueError('BEAR does not support discrete action-space.')
-        else:
-            return BEAR(**params)
-    elif name == 'cql':
-        if discrete:
-            return DiscreteCQL(**params)
-        else:
-            return CQL(**params)
-    elif name == 'ddpg':
-        if discrete:
-            raise ValueError('DDPG does not support discrete action-space.')
-        else:
-            return DDPG(**params)
-    elif name == 'dqn':
-        if discrete:
-            return DQN(**params)
-        else:
-            raise ValueError('DQN does not support continuous action-space.')
-    elif name == 'double_dqn':
-        if discrete:
-            return DoubleDQN(**params)
-        else:
-            raise ValueError(
-                'DoubleDQN does not support continuous action-space.')
-    elif name == 'plas':
-        if discrete:
-            raise ValueError('PLAS does not support discrete action-space.')
-        else:
-            return PLAS(**params)
-    elif name == 'sac':
-        if discrete:
-            return DiscreteSAC(**params)
-        else:
-            return SAC(**params)
-    elif name == 'td3':
-        if discrete:
-            raise ValueError('TD3 does not support discrete action-space.')
-        else:
-            return TD3(**params)
-    raise ValueError('invalid algorithm name.')
+    return get_algo(name, discrete)(**params)
