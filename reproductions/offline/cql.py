48b14023f054d1d9ee1f3dcb2298d8bad98de041
takuseno <takuma.seno@gmail.com>
2021-01-22 20:56:08 +0900

    Update default learning rates of CQL based on paper

--- reproductions/offline/cql.py
+++ reproductions/offline/cql.py
@@ -1,12 +1,19 @@
 from d3rlpy.algos import CQL
 from d3rlpy.datasets import get_d4rl
 from d3rlpy.metrics.scorer import evaluate_on_environment
+from d3rlpy.metrics.scorer import average_value_estimation_scorer
+from sklearn.model_selection import train_test_split
 
-dataset, env = get_d4rl('hopper-medium-replay-v0')
+dataset, env = get_d4rl('hopper-medium-v0')
+
+_, test_episodes = train_test_split(dataset, test_size=0.2)
 
 cql = CQL(use_gpu=True)
 
 cql.fit(dataset.episodes,
-        eval_episodes=dataset.episodes,
+        eval_episodes=test_episodes,
         n_epochs=2000,
-        scorers={'environment': evaluate_on_environment(env)})
+        scorers={
+            'environment': evaluate_on_environment(env),
+            'value_scale': average_value_estimation_scorer
+        })
