d5343fccdc672890d602478cbd61007a86f3c153
takuseno <takuma.seno@gmail.com>
2021-01-24 17:36:59 +0900

    Add test for ChannelFirst

--- tests/envs/test_wrappers.py
+++ tests/envs/test_wrappers.py
@@ -1,5 +1,6 @@
 import gym
 
+from gym.wrappers import AtariPreprocessing
 from d3rlpy.algos import DQN
 from d3rlpy.envs.wrappers import ChannelFirst
 
@@ -18,6 +19,27 @@ def test_channel_first():
     # check step
     observation, _, _, _ = wrapper.step(wrapper.action_space.sample())
     assert observation.shape == (channel, width, height)
+
+    # check with algorithm
+    dqn = DQN()
+    dqn.build_with_env(wrapper)
+    dqn.predict([observation])
+
+
+def test_channel_first_with_2_dim_obs():
+    env = AtariPreprocessing(gym.make("BreakoutNoFrameskip-v4"))
+
+    width, height = env.observation_space.shape
+
+    wrapper = ChannelFirst(env)
+
+    # check reset
+    observation = wrapper.reset()
+    assert observation.shape == (1, width, height)
+
+    # check step
+    observation, _, _, _ = wrapper.step(wrapper.action_space.sample())
+    assert observation.shape == (1, width, height)
 
     # check with algorithm
     dqn = DQN()
