6d65134498bf3d0ab3474107ec43c03d25050fe0
takuseno <takuma.seno@gmail.com>
2021-06-16 01:07:29 +0900

    Change default mini-batch size

--- d3rlpy/algos/td3_plus_bc.py
+++ d3rlpy/algos/td3_plus_bc.py
@@ -105,7 +105,7 @@ class TD3PlusBC(AlgoBase):
         actor_encoder_factory: EncoderArg = "default",
         critic_encoder_factory: EncoderArg = "default",
         q_func_factory: QFuncArg = "mean",
-        batch_size: int = 100,
+        batch_size: int = 256,
         n_frames: int = 1,
         n_steps: int = 1,
         gamma: float = 0.99,
