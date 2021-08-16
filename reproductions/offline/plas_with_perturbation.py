cdd5f4d390153565437a98aa8b9eb96e715af0c2
takuseno <takuma.seno@gmail.com>
2021-08-16 18:59:03 +0900

    Update PLAS+P reproduction

--- reproductions/offline/plas_with_perturbation.py
+++ reproductions/offline/plas_with_perturbation.py
@@ -22,19 +22,21 @@ def main():
         vae_encoder = d3rlpy.models.encoders.VectorEncoderFactory([128, 128])
     else:
         vae_encoder = d3rlpy.models.encoders.VectorEncoderFactory([750, 750])
-
     encoder = d3rlpy.models.encoders.VectorEncoderFactory([400, 300])
 
     plas = d3rlpy.algos.PLASWithPerturbation(
         actor_encoder_factory=encoder,
         critic_encoder_factory=encoder,
         imitator_encoder_factory=vae_encoder,
+        lam=1.0,
+        warmup_steps=500000,
+        action_flexibility=0.05,
         use_gpu=args.gpu)
 
     plas.fit(
         dataset.episodes,
         eval_episodes=test_episodes,
-        n_steps=500000,
+        n_steps=1000000,  # RL starts at 500000 step
         n_steps_per_epoch=1000,
         save_interval=10,
         scorers={
