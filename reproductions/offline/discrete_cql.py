ff166e608e663aa7d3e3f405b09d2ba245b51493
takuseno <takuma.seno@gmail.com>
2021-11-18 17:56:05 +0900

    Update DiscreteCQL reproduction code

--- reproductions/offline/discrete_cql.py
+++ reproductions/offline/discrete_cql.py
@@ -1,41 +1,44 @@
 import argparse
 import d3rlpy
-from sklearn.model_selection import train_test_split
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', type=str, default='breakout-medium-v0')
+    parser.add_argument('--game', type=str, default='breakout')
     parser.add_argument('--seed', type=int, default=1)
     parser.add_argument('--gpu', type=int)
     args = parser.parse_args()
 
-    dataset, env = d3rlpy.datasets.get_atari(args.dataset)
-
-    # fix seed
     d3rlpy.seed(args.seed)
+
+    dataset, env = d3rlpy.datasets.get_atari_transitions(args.game,
+                                                         fraction=0.01)
+
     env.seed(args.seed)
 
-    _, test_episodes = train_test_split(dataset, test_size=0.2)
-
     cql = d3rlpy.algos.DiscreteCQL(
+        learning_rate=5e-5,
         optim_factory=d3rlpy.models.optimizers.AdamFactory(eps=1e-2 / 32),
-        scaler='pixel',
+        batch_size=32,
+        alpha=4.0,
+        q_func_factory=d3rlpy.models.q_functions.QRQFunctionFactory(
+            n_quantiles=200),
+        scaler="pixel",
         n_frames=4,
-        q_func_factory='qr',
+        target_update_interval=2000,
+        reward_scaler=d3rlpy.preprocessing.ClipRewardScaler(-1.0, 1.0),
         use_gpu=args.gpu)
 
     env_scorer = d3rlpy.metrics.evaluate_on_environment(env, epsilon=0.001)
 
-    cql.fit(dataset.episodes,
-            eval_episodes=test_episodes,
-            n_steps=50000000,
-            n_steps_per_epoch=10000,
+    cql.fit(dataset,
+            eval_episodes=[None],
+            n_steps=50000000 // 4,
+            n_steps_per_epoch=125000,
             scorers={
                 'environment': env_scorer,
-                'value_scale': d3rlpy.metrics.average_value_estimation_scorer,
             },
-            experiment_name=f"DiscreteCQL_{args.dataset}_{args.seed}")
+            experiment_name=f"DiscreteCQL_{args.game}_{args.seed}")
 
 
 if __name__ == '__main__':
