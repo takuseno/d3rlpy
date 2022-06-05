4da9698e541db0f799c292ab06b42532d3ebfb9a
takuseno <takuma.seno@gmail.com>
2022-06-05 14:55:35 +0900

    Fix unit test

--- tests/test_gpu.py
+++ tests/test_gpu.py
@@ -1,24 +1,6 @@
-import copy
-from unittest.mock import patch
-
-from d3rlpy.context import parallel
 from d3rlpy.gpu import Device
 
 
-@patch("d3rlpy.gpu.get_gpu_count", return_value=2)
-def test_device(mock):
+def test_device():
     device = Device()
-
-    copy_device = copy.deepcopy(device)
     assert device.get_id() == 0
-    assert copy_device.get_id() == 0
-
-    with parallel():
-        inc_device = copy.deepcopy(device)
-        assert device.get_id() == 1
-        assert inc_device.get_id() == 1
-
-        # check circulation
-        inc2_device = copy.deepcopy(device)
-        assert device.get_id() == 0
-        assert inc2_device.get_id() == 0
