cc92e69baccd93e2026f39f637ca58edee2b745e
takuseno <takuma.seno@gmail.com>
2022-07-19 00:51:13 +0900

    Remove Cython build

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, find_packages
 
 os.environ['CFLAGS'] = '-std=c++11'
 
@@ -9,24 +9,6 @@ here = os.path.abspath(os.path.dirname(__file__))
 exec(open(os.path.join(here, 'd3rlpy', '_version.py')).read())
 
 if __name__ == "__main__":
-    from numpy import get_include
-    from Cython.Build import cythonize
-
-    # setup Cython build
-    ext = Extension('d3rlpy.dataset',
-                    sources=['d3rlpy/dataset.pyx'],
-                    include_dirs=[get_include(), 'd3rlpy/cpp/include'],
-                    language='c++',
-                    extra_compile_args=["-std=c++11", "-O3", "-ffast-math"],
-                    extra_link_args=["-std=c++11"])
-
-    ext_modules = cythonize([ext],
-                            compiler_directives={
-                                'linetrace': True,
-                                'binding': True
-                            })
-
-    # main setup
     setup(name="d3rlpy",
           version=__version__,
           description="An offline deep reinforcement learning library",
@@ -62,11 +44,5 @@ if __name__ == "__main__":
                             "colorama"],
           packages=find_packages(exclude=["tests*"]),
           python_requires=">=3.7.0",
-          zip_safe=False,
-          package_data={'d3rlpy': ['*.pyx',
-                                   '*.pxd',
-                                   '*.h',
-                                   '*.pyi',
-                                   'py.typed']},
-          ext_modules=ext_modules,
+          zip_safe=True,
           entry_points={'console_scripts': ['d3rlpy=d3rlpy.cli:cli']})
