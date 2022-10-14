d3239cc567648451c37b3ed0ac4dee492d6dcbb9
takuseno <takuma.seno@gmail.com>
2022-10-14 22:11:11 +0900

    Drop model-based RL support

--- d3rlpy/algos/torch/__init__.py
+++ d3rlpy/algos/torch/__init__.py
@@ -2,7 +2,6 @@ from .awac_impl import AWACImpl
 from .bc_impl import BCImpl, DiscreteBCImpl
 from .bcq_impl import BCQImpl, DiscreteBCQImpl
 from .bear_impl import BEARImpl
-from .combo_impl import COMBOImpl
 from .cql_impl import CQLImpl, DiscreteCQLImpl
 from .crr_impl import CRRImpl
 from .ddpg_impl import DDPGImpl
