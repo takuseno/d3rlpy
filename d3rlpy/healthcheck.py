6bcabe1ac37fe23da0859730048174e72b7dadaa
takuseno <takuma.seno@gmail.com>
2023-07-29 17:19:43 +0900

    Replace warning with exception at gym check

--- d3rlpy/healthcheck.py
+++ d3rlpy/healthcheck.py
@@ -1,5 +1,3 @@
-import warnings
-
 __all__ = ["run_healthcheck"]
 
 
@@ -10,8 +8,8 @@ def run_healthcheck() -> None:
 def _check_gym() -> None:
     from gym.version import VERSION
 
-    if VERSION < "0.26.2":
-        warnings.warn(
-            f"gym=={VERSION} is outdated. "
-            "Please upgrade Gym to 0.26.2 or later."
+    if VERSION < "0.26.0":
+        raise ValueError(
+            "Gym version is too outdated. "
+            "Please upgrade Gym to 0.26.0 or later."
         )
