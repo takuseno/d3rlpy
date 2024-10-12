4d0c82c836901b0b8dd334f11270525979969f14
takuseno <takuma.seno@gmail.com>
2024-10-12 15:38:31 +0900

    Add Gymnasium version check

--- d3rlpy/healthcheck.py
+++ d3rlpy/healthcheck.py
@@ -6,10 +6,17 @@ def run_healthcheck() -> None:
 
 
 def _check_gym() -> None:
+    import gymnasium
     from gym.version import VERSION
 
     if VERSION < "0.26.0":
         raise ValueError(
             "Gym version is too outdated. "
             "Please upgrade Gym to 0.26.0 or later."
+        )
+
+    if gymnasium.__version__ < "1.0.0":
+        raise ValueError(
+            "Gymnasium version is too outdated. "
+            "Please upgrade Gymnasium to 1.0.0 or later."
         )
